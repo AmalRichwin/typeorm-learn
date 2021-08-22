9c74e7832da22b5f43cc78f61d9a414e934dcbfb
Richwin_ <77601299+AmalRichwin-AT@users.noreply.github.com>
2021-08-22 12:01:54 +0530

    update banker.ts

--- src/entities/Banker.ts
+++ src/entities/Banker.ts
@@ -40,28 +40,6 @@ export class Banker extends BaseEntity {
   })
   balance: number;
 
-  @Column({
-    default: true,
-    name: 'active',
-  })
-  is_active: boolean;
-
-  @Column({
-    type: 'simple-json',
-    nullable: true,
-  })
-  additional_info: {
-    age: number;
-    gender: string;
-    account_type: string;
-  };
-
-  @Column({
-    type: 'simple-array',
-    default: [],
-  })
-  family_members: string[];
-
   @CreateDateColumn()
   created_at: Date;
 
